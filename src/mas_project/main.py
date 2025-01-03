4a0185226b5d5c705f9e4e8e6090cac964f11091
thepetk <thepetk@gmail.com>
2025-01-03 18:54:26 +0000

    Update flow

--- src/mas_project/main.py
+++ src/mas_project/main.py
@@ -1,52 +1,67 @@
 #!/usr/bin/env python
-from operator import or_
+import json
 import os
 
 from pydantic import BaseModel
 
-from crewai.flow.flow import Flow, listen, start, router
+from crewai.flow.flow import Flow, listen, start, router, or_
 
-from .crews.emergency_services import EmergencyServicesCrew
-from .crews.firefighter_services import FirefighterServicesCrew
-from .crews.medical_services import MedicalServicesCrew
+from mas_project.crews.emergency_services import EmergencyServicesCrew
+from mas_project.crews.firefighter_services import FirefighterServicesCrew
+from mas_project.crews.medical_services import MedicalServicesCrew
 
-FIRE_REPORT_PATH: str = os.getenv("FIRE_REPORT_PATH", "fire_report.mdx")
+FIRE_REPORT_PATH: str = os.getenv("FIRE_REPORT_PATH", "emergency_01.mdx")
 
 
 class FireEventState(BaseModel):
-    location_x: int
-    location_y: int
-    injured_people: int
-    fire_type: str
-    fire_severity: str
-    assigned_medical_unit_id: int
-    assigned_firefighter_unit_id: int
-    status: str
+    """
+    handles the state of the fire event on
+    every step of the FireEventFlow object.
+    """
+
+    location_x: "float" = None
+    location_y: "float" = None
+    injured_people: "int" = None
+    fire_type: "str" = None
+    fire_severity: "str" = None
+    assigned_medical_unit_id: "int" = None
+    assigned_firefighter_unit_id: "int" = None
+    status: str = "initialized"
 
 
 class FireEventFlow(Flow[FireEventState]):
+    """
+    the main drive behind the functionality of the fire
+    response mechanism. Combines the functionality of
+    the 3 project crews (emergency, firefighter and med-
+    ical services). Uses a router to decide which checks
+    if the medical services are needed or not.
+
+    """
+
     @start()
-    def read_fire_event_report(self):
-        result = (
+    def read_fire_event_report(self) -> "None":
+        _r = (
             EmergencyServicesCrew()
             .crew()
             .kickoff(inputs={"mdx_file": FIRE_REPORT_PATH})
         )
-        self.state.location_x = result.pydantic.location_x
-        self.state.location_y = result.pydantic.location_y
-        self.state.injured_people = result.pydantic.injured_people
-        self.state.fire_type = result.pydantic.fire_type
-        self.state.fire_severity = result.pydantic.fire_severity
+        result = json.loads(_r.raw)
+        self.state.location_x = result["location_x"]
+        self.state.location_y = result["location_y"]
+        self.state.injured_people = result["injured_people"]
+        self.state.fire_type = result["fire_type"]
+        self.state.fire_severity = result["fire_severity"]
         self.state.status = "reported"
 
     @router(read_fire_event_report)
-    def check_for_injured_people(self):
+    def check_for_injured_people(self) -> "str":
         if self.state.injured_people > 0:
             return "Has Injured People"
         else:
             return "No Injured People"
 
-    def _assign_medical_units(self) -> int:
+    def _assign_medical_units(self) -> "int":
         _r = (
             MedicalServicesCrew()
             .crew()
@@ -59,39 +74,38 @@ class FireEventFlow(Flow[FireEventState]):
                 }
             )
         )
-        return _r.pydantic.uid
+        return _r.raw["uid"]
 
-    def _dispatch_firefighter_units(self) -> int:
+    def _dispatch_firefighter_units(self) -> "int":
         _r = (
             FirefighterServicesCrew()
             .crew()
-            .kickoff(
-                inputs={
-                    "json_file": FIRE_REPORT_PATH,
-                    "location_x": self.state.location_x,
-                    "location_y": self.state.location_y,
-                    "fire_type": self.state.fire_type,
-                    "fire_severity": self.state.fire_severity,
+            .kickoff(inputs={
+                    "json_file": 'firetrucks.json',
+                    "location_x": 28.461720670375886,
+                    "location_y": -16.2835862728014,
+                    "fire_type": "ordinary",
+                    "fire_severity": "high",
                 }
             )
         )
-        return _r.pydantic.uid
+        return _r.raw["uid"]
 
     @listen("Has Injured People")
-    def handle_event_with_injured_people(self):
+    def handle_event_with_injured_people(self) -> "None":
         self.state.assigned_firefighter_unit_id = self._dispatch_firefighter_units()
         self.state.assigned_medical_unit_id = self._assign_medical_units()
         self.state.status = "both_units_assigned"
 
     @listen("No Injured People")
-    def handle_event_without_injured_people(self):
+    def handle_event_without_injured_people(self) -> "None":
         self.state.assigned_firefighter_unit_id = self._dispatch_firefighter_units()
         self.state.assigned_medical_unit_id = None
 
         self.state.status = "only_firefighter_units_assigned"
 
     @listen(or_(handle_event_with_injured_people, handle_event_without_injured_people))
-    def print_response_scenario(self):
+    def print_response_scenario(self) -> "None":
         print(
             f"Assigned Firefighter Unit ID: {self.state.assigned_firefighter_unit_id}"
         )
@@ -101,7 +115,7 @@ class FireEventFlow(Flow[FireEventState]):
             print(f"Assigned Medical Unit ID: Not Needed")
 
 
-def kickoff():
+def kickoff() -> "None":
     poem_flow = FireEventFlow()
     poem_flow.kickoff()
 
